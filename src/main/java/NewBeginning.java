bf505d660f4775f666da127ef354dbc941c618e7
Diana Migasiuk <diana.migasiuk@contractors.roche.com>
2019-04-26 12:32:49 +0200

    shoot me

--- src/main/java/NewBeginning.java
+++ src/main/java/NewBeginning.java
@@ -1,6 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class NewBeginning {
+
+    public enum Test {
+        Test("Teścik"),
+        NoOne("Znowu nie pamiętam");
+
+        String name;
+
+        Test(String name) {
+            this.name = name;
+        }
+
+        public void sayHello() {
+            System.out.println("hello: " + name);
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Siemanko!");
+
+        Test.valueOf("NoOne").sayHello();
+        List<Test> tests = Arrays.asList(Test.Test, Test.NoOne);
+
+        Test test = null;
+        if (tests.contains(test)) {
+            System.out.println("blabla");
+        }
     }
 }
